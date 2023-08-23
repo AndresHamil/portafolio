a13f608eec8d2b1fdb8bb957ce2b294adf92beb4
Luis Andres Rodriguez Campos <luis.andres.rodriguez.campos0709@gmail.com>
2023-08-22 18:46:55 -0600

    Usando dependencias de terceros, implemente una libreria de iconos

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
+import { MdAccountCircle } from "react-icons/md";
 import "./post.css";
 
 export const Post = ({ title, body }) => {
   return (
     <>
       <div className="box">
-        <div className="tittle">{title}</div>
+        <div className="header">
+            <div className="icon">
+                <MdAccountCircle />
+            </div>
+            <div>
+                {title}
+            </div>
+        </div>
         <div className="body">{body}</div>
       </div>
     </>
