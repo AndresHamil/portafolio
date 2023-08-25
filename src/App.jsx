10706c87854ea511819620b3c72e8d97e9be3611
Luis Andres Rodriguez Campos <luis.andres.rodriguez.campos0709@gmail.com>
2023-08-24 22:34:33 -0600

    Se termino el componente 'Presentacion'

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 
 import { Publicaciones } from "./Publicaciones";
 
@@ -11,6 +11,28 @@ export const App = () => {
     const [titulo, setTitulo] = useState(null);
     const [descripcion, setDescripcion] = useState(null);
     const [publicaciones, setPublicaciones] = useState([]);
+
+    const [data, setData] = useState([
+      {
+        presentacion: {
+          titulo: 'Programador Frontend & Backend',
+          texto: 'Combinando innovación visual con robustez técnica en un mundo digital.',
+          img: 'https://scontent.ftrc2-1.fna.fbcdn.net/v/t39.30808-6/333269803_1256894074896685_7049032288644285287_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeHcUyY8_7tTSM7KoEDCmIzF2OB8FnjuxI7Y4HwWeO7EjvScUdofZ7j4XLhORRVdYRjUgjKQW_bqlgbxSa7R9guD&_nc_ohc=FV48XrzNyKwAX_qWX1Z&_nc_ht=scontent.ftrc2-1.fna&oh=00_AfCf1pSs64hRmeBuYW0yBJqw0VPFatBjiS3kUu0pWKeBXQ&oe=64ECA966'
+        }
+      }
+    ]);
+
+    useEffect(() => {
+      // fetch("https://jsonplaceholder.typicode.com/posts")
+      //   .then((response) => response.json())
+      //   .then((data) => setPosts(data))
+      //   .catch((error) => console.log(error));
+      // consultarInfomacion()
+    }, []);
+
+    function consultarInfomacion(){
+      alert('Entro')
+    }
 
     function agregarTarea(){
         const nuevaPublicacion = {
@@ -42,7 +64,11 @@ export const App = () => {
             )
         })
       } */}
-      <Presentacion/>
+      <Presentacion
+        titulo={data[0].presentacion.titulo}
+        texto={data[0].presentacion.texto}
+        img={data[0].presentacion.img}
+      />
     </div>
   );
 };
--- src/components/presentacion/Presentacion.jsx
+++ src/components/presentacion/Presentacion.jsx
@@ -1,16 +1,34 @@
-import "./presentacion.scss"
+import "./presentacion.scss";
 
-export const Presentacion = () => {
-    return(
-        <>
-            <div className="box">
-                <div className="titulo">
-                    <h1>Programador Frontend & Backend</h1>
-                </div>
-                <div className="text">
-                    <span>Combinando innovación visual con robustez técnica en un mundo digital.</span>
-                </div>
-            </div>
-        </>
-    )
-}
+export const Presentacion = ({ titulo, texto, img }) => {
+  return (
+    <>
+      <div className="box">
+        <div className="presentacion-titulo">
+          <h1>{titulo}</h1>
+        </div>
+        <div className="presentacion-text">
+          <span>{texto}</span>
+        </div>
+        <div className="presentacion-panel">
+          <div className="circle">
+            <img src={img} alt="" />
+          </div>
+        </div>
+        <div className="presentacion-infomacion">
+          <span>
+            <h1>Hola, soy Andres. Encantado de conocerlo.</h1>
+            <h2>
+              Desde mis primeros pasos en programación, me han fascinado los
+              desafíos y la creación de soluciones innovadoras. Mi verdadera
+              pasión reside en transformar ideas en aplicaciones estéticamente
+              atractivas y funcionalmente eficientes. Con cada línea de código,
+              busco la perfección, y con cada proyecto, avanzo en mi constante
+              búsqueda de la excelencia en el arte de programar.
+            </h2>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
