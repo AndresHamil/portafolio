8490addb79f7472e4bd0099e737e1b019566c7d1
Luis Andres Rodriguez Campos <luis.andres.rodriguez.campos0709@gmail.com>
2024-01-30 22:45:12 -0600

    Reaunude proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,18 @@ export const App = () => {
     const [data, setData] = useState([
       {
         presentacion: {
-          titulo: 'Programador Frontend & Backend',
+          titulo: 'Programador Frontend & Backendd',
           texto: 'Combinando innovación visual con robustez técnica en un mundo digital.',
-          img: 'https://scontent.ftrc2-1.fna.fbcdn.net/v/t39.30808-6/333269803_1256894074896685_7049032288644285287_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeHcUyY8_7tTSM7KoEDCmIzF2OB8FnjuxI7Y4HwWeO7EjvScUdofZ7j4XLhORRVdYRjUgjKQW_bqlgbxSa7R9guD&_nc_ohc=FV48XrzNyKwAX_qWX1Z&_nc_ht=scontent.ftrc2-1.fna&oh=00_AfCf1pSs64hRmeBuYW0yBJqw0VPFatBjiS3kUu0pWKeBXQ&oe=64ECA966'
+          img: 'https://scontent.ftrc2-1.fna.fbcdn.net/v/t39.30808-6/333269803_1256894074896685_7049032288644285287_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeHcUyY8_7tTSM7KoEDCmIzF2OB8FnjuxI7Y4HwWeO7EjvScUdofZ7j4XLhORRVdYRjUgjKQW_bqlgbxSa7R9guD&_nc_ohc=FV48XrzNyKwAX_qWX1Z&_nc_ht=scontent.ftrc2-1.fna&oh=00_AfCf1pSs64hRmeBuYW0yBJqw0VPFatBjiS3kUu0pWKeBXQ&oe=64ECA966',
+          infomacion: {
+            titulo: 'Hola, soy Andres. Encantado de conocerlo.',
+            texto: `Desde mis primeros pasos en programación, me han fascinado los
+            desafíos y la creación de soluciones innovadoras. Mi verdadera
+            pasión reside en transformar ideas en aplicaciones estéticamente
+            atractivas y funcionalmente eficientes. Con cada línea de código,
+            busco la perfección, y con cada proyecto, avanzo en mi constante
+            búsqueda de la excelencia en el arte de programar.`
+          }
         }
       }
     ]);
@@ -68,7 +77,25 @@ export const App = () => {
         titulo={data[0].presentacion.titulo}
         texto={data[0].presentacion.texto}
         img={data[0].presentacion.img}
+        infomacion={data[0].presentacion.infomacion}
       />
+      <div className="panel-cards">
+        <div className="panel-card-box">
+          d
+          {/* <div className="panel-card-box-card">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate perspiciatis, magnam magni quos amet tempora libero consequatur. Expedita numquam fugiat quis iste quia optio dolorem illum at dolore, doloribus facere?
+          </div>
+
+          <div className="panel-card-box-card">
+            ddd
+          </div>
+
+          <div className="panel-card-box-card">
+            lol
+            
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/presentacion/Presentacion.jsx
+++ src/components/presentacion/Presentacion.jsx
@@ -1,6 +1,6 @@
 import "./presentacion.scss";
 
-export const Presentacion = ({ titulo, texto, img }) => {
+export const Presentacion = ({ titulo, texto, img,infomacion }) => {
   return (
     <>
       <div className="box">
@@ -17,17 +17,17 @@ export const Presentacion = ({ titulo, texto, img }) => {
         </div>
         <div className="presentacion-infomacion">
           <span>
-            <h1>Hola, soy Andres. Encantado de conocerlo.</h1>
+            <h1>{infomacion.titulo}</h1>
             <h2>
-              Desde mis primeros pasos en programación, me han fascinado los
-              desafíos y la creación de soluciones innovadoras. Mi verdadera
-              pasión reside en transformar ideas en aplicaciones estéticamente
-              atractivas y funcionalmente eficientes. Con cada línea de código,
-              busco la perfección, y con cada proyecto, avanzo en mi constante
-              búsqueda de la excelencia en el arte de programar.
+                {infomacion.texto}
             </h2>
           </span>
         </div>
+        {/* <div className="divPadre">
+            <div className="divHijo">Holaaa</div>
+            <div className="divHijo">Pero</div>
+            <div className="divHijo">Looookl</div>
+        </div> */}
       </div>
     </>
   );
