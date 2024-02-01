0970f87078be11b6b12da8e1aeb9c74f13a64ab9
Luis Andres Rodriguez Campos <luis.andres.rodriguez.campos0709@gmail.com>
2024-01-31 23:40:22 -0600

    ejemplo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './styles.scss';
 
@@ -6,7 +7,23 @@ const Header = () => {
 
     return (
         <>
-            <label htmlFor="">header</label>
+            <div className="and-header">
+                <div className="and-header_content">
+                    <div className="and-header_content_nav">
+                        <ul>
+                            <li><NavLink to="/portafolio"><span>Acerca</span></NavLink></li>
+                            <li><NavLink to="/portafolio"><span>Habilidades</span></NavLink></li>
+                            <li><NavLink to="/portafolio"><span>Educacion</span></NavLink></li>
+                            <li><NavLink to="/portafolio"><span>Lenguajes</span></NavLink></li>
+                            <li><NavLink to="/portafolio"><span>Trayectoria</span></NavLink></li>
+                            <li><NavLink to="/portafolio"><span>Contacto</span></NavLink></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="and-header_menu">
+                    Hola
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import './styles.scss';
 
-// COMPONENETES
-
+// COMPONENTES
 import Header from '../../components/header/index';
 
-const Index = () => {
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
+  const moveButton = (event) => {
+    const button = event.target;
+    const randomX = getRandomInt(-100, 100); // Números aleatorios entre -500 y 500
+    const randomY = getRandomInt(-50, 400); // Números aleatorios entre -50 y 500
+    button.style.transform = `translate(${randomX}px, ${randomY}px)`;
+  };
 
-    return (
-        <>
-            <Header />
-            
-        </>
-    );
-}
+  const saludo = () => {
+    alert('YA SABIA QUE SI MI AMOR ❤️')
+  }
+
+const Index = () => {
+  return (
+    <>
+      <Header />
+
+      <div className="titulo">
+        Hola
+      </div>
+
+      <div className="panel">
+        <div className="button no" onMouseOver={moveButton} onClick={moveButton}>
+          no
+        </div>
+        <div className="button si" onClick={saludo}>
+          Si
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Index;
