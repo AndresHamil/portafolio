5f0c5e94eb5f8796d801b4350f0d12360f2336f6
Luis Andres Rodriguez Campos <luis.andres.rodriguez.campos0709@gmail.com>
2024-02-25 14:59:38 -0600

    Se agregaron avances del header

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import './styles.scss';
 
 // COMPONENTES
@@ -6,7 +7,6 @@ import Header from '../../components/header/index';
 
 const Index = () => {
   useEffect(() => {
-    // Comprobamos si el dispositivo soporta la API de DeviceOrientation
     if (window.DeviceOrientationEvent) {
       // Agregamos un listener para el evento de orientación
       window.addEventListener('deviceorientation', handleOrientation);
@@ -14,24 +14,20 @@ const Index = () => {
       console.log('El dispositivo no soporta la API de DeviceOrientation.');
     }
 
-    // Función que maneja el evento de orientación
     function handleOrientation(event) {
-      const beta = event.beta; // ángulo de inclinación alrededor del eje x
+      const beta = event.beta; 
+      // if (beta > 90) {
+      //   alert('¡Giraste el celular hacia abajo!');
+      // }
 
-      // Comprobamos si el dispositivo está inclinado hacia abajo
-      if (beta > 90) {
-        // Mostramos una alerta indicando que el dispositivo está inclinado hacia abajo
-        alert('¡Giraste el celular hacia abajo!');
-        
-        // Puedes realizar otras acciones aquí según tus necesidades
-      }
+      setRote(beta)
     }
-
-    // Limpiamos el listener cuando el componente se desmonta
     return () => {
       window.removeEventListener('deviceorientation', handleOrientation);
     };
-  }, []); // El segundo argumento del useEffect es un array de dependencias, que está vacío para que se ejecute solo una vez al montar el componente
+  }, []); 
+
+  const [rote, setRote] = useState(null);
 
   return (
     <>
@@ -47,6 +43,7 @@ const Index = () => {
           <label htmlFor="">Andres Rodriguez</label>
           <br />
           <span>Desarrollador full stack</span>
+          <span>{rote}</span>
         </div>
       </div>
     </>
